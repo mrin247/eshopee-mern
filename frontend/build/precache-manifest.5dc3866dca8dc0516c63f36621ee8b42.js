d9a2c6a00eb2b3f6bff137778dd48084505f362b
Mrin <mrinmoym247@gmail.com>
2022-12-02 11:25:48 +0530

    Deploy v0.0.1

---
+++ frontend/build/precache-manifest.5dc3866dca8dc0516c63f36621ee8b42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cb349813bf92c2e99ebdc5b38732354",
+    "url": "/index.html"
+  },
+  {
+    "revision": "129329305b60f6b33431",
+    "url": "/static/css/main.62b7980e.chunk.css"
+  },
+  {
+    "revision": "74f77146fc36a4722793",
+    "url": "/static/js/2.2ffe1190.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2ffe1190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "129329305b60f6b33431",
+    "url": "/static/js/main.7a43271a.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
